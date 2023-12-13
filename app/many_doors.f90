d2f7127932b96dd3aa4da4645f71b628cacd843a
Patrick Raynaud <patrick.raynaud@nrc.gov>
2023-12-12 21:15:57 -0500

    create(program): many_doors

---
+++ app/many_doors.f90
@@ -0,0 +1,12 @@
+program many_doors
+  use doors_m, only: get_open_doors
+  implicit none
+
+  integer, parameter :: number_of_doors = 100
+  integer, parameter :: number_of_passes = 100
+  integer, dimension(:), allocatable :: open_doors
+
+  open_doors = get_open_doors(number_of_doors, number_of_passes)
+
+  print *, "Open Doors: ", open_doors
+end program many_doors
